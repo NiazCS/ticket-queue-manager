c092b3620f7aa50c87e005217eafe9f937704daf
NiazCS <niazhussain123456@gmail.com>
2025-05-14 02:38:54 -0400

    Update Ticket.h

--- Ticket.h
+++ Ticket.h
@@ -1,7 +1,4 @@
 //ticket.h
-//niaz hussain
-//ticket queue manager
-//cst 202 – data structures
 
 #ifndef TICKET_H
 #define TICKET_H
@@ -11,7 +8,7 @@
 
 struct Ticket {
     int id;    // unique ticket number
-    std::string description; // user’s issue
+    std::string description; // userâ€™s issue
     std::chrono::time_point<std::chrono::steady_clock> timestamp; // creation time
 };
 
